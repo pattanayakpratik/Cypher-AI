b6e0232492d6e09ea9bc8a94991abb84d581b303
Pratik Pattanayak <mr.pratikpattanayak@gmail.com>
2026-01-23 00:40:05 +0530

    Delete project directory

--- project/musicLibrary.py
+++
@@ -1,6 +0,0 @@
-music={
-    # ad your fav music
-    "ishq": "https://youtu.be/hHuG7FIKgtc?si=5D8_giJycmTg1SaJ",
-    "halka halka" : "https://youtu.be/glejzVr2oPQ?si=EsHMlaG0CCFlHdeC",
-    "saadgi": "https://youtu.be/K-iaJKCEp0c?si=Y9Oy7mTmPdb06_Q9"
-}
